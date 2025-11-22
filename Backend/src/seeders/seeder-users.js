5eb301734e76a8289e906997a9e5d70814f46a05
Xuan Son <buixu4ns0n@gmail.com>
2025-11-22 08:12:13 +0700

    [db] seed

---
+++ Backend/src/seeders/seeder-users.js
@@ -0,0 +1,33 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          firstName: "Xuan",
+          lastName: "Son",
+          email: "admin@gmail.com",
+          password: "123456",
+          address: "Hanoi",
+          gender: 1,
+          typeRole: "ROLE",
+          keyRole: "R1",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
